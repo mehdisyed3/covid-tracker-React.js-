9da35e21fe65a90eeccb7dfae430ea8761891210
Syed Mehdi <mehdisyed3@gmail.com>
2020-11-23 18:23:44 -0500

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Welcome to Covied Tracker</h1>
+
+      {/* 
+        header : Title  + Select input dropdown field 
+      */}
+
+      {/* 3 info box components */}
+
+      {/* Table */}
+      {/* Graph */}
+      {/* Maps */}
+
+
     </div>
+
   );
 }
 
