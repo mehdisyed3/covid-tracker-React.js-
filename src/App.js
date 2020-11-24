e615a1668f4fb6fae29d825d4f6479b71812e33a
Syed Mehdi <mehdisyed3@gmail.com>
2020-11-23 20:38:22 -0500

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-
 import './App.css';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState('World Wide')
+
+
+  const menuItems = countries.map((item, i) => <MenuItem value={item.value} key={i} >{item.name}</MenuItem>)
+
+
+
+  useEffect(() => {
+
+    const getCountries = async () => {
+
+      await fetch("https://disease.sh/v3/covid-19/countries")
+            .then((res) => res.json())
+            .then((data) => {
+              const dt = data.map(item => ({
+                name: item.country,
+                value: item.countryInfo.iso2
+              }))
+
+            setCountries(dt)
+        })
+    }
+    getCountries()
+  }, [])
+
+  const onChangeCountry = (e)=>{
+      
+    const selectedCountry = e.target.value
+    setCountry(selectedCountry)
+
+  }
+
+
   return (
     <div className="app">
-      <h1>Welcome to Covied Tracker</h1>
+      <div className="app__header">
+
+        <h1>Covid Tracker</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' value={country} onChange={onChangeCountry} >
+          <MenuItem value='World wide' > World Wide </MenuItem>
+            {menuItems}
+
+          </Select>
+        </FormControl>
+
+      </div>
+      <div className='info__stats'>
+
+      </div>
+
+
 
       {/* 
         header : Title  + Select input dropdown field 
@@ -12,8 +63,8 @@ function App() {
 
       {/* 3 info box components */}
 
-      {/* Table */}
       {/* Graph */}
+      {/* Table */}
       {/* Maps */}
 
 
